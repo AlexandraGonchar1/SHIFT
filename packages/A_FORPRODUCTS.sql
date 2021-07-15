47ae9c5ba6adb09fb8149f1565e47690bcb909bd
AlexandraGonchar1 <87025265+AlexandraGonchar1@users.noreply.github.com>
2021-07-16 00:57:18 +0700

    Update A_FORPRODUCTS.sql

--- packages/A_FORPRODUCTS.sql
+++ packages/A_FORPRODUCTS.sql
@@ -11,11 +11,11 @@ create or replace NONEDITIONABLE package A_ForProducts as
 
     procedure TransferProductStorage (
       p_id_product_storage number,
-      p_id_storage number
+      p_id_storage number,
+      volume_pr1 integer
       );
      procedure NewProductStorage(
       p_volume_pr          a_products_storage.volume_pr%type,
-      p_product_storage              number,
       p_storage           number,
       p_product           number);
 end A_ForProducts; --спец
@@ -51,15 +51,24 @@ begin
   exception
     when others then dbms_output.put_line('smth wrong((');
   end; 
+procedure productMinus(p_storage a_products_storage.id_storage%type,
+                       product a_products_storage.id_product_storage%type,
+                       volume_pr1 integer) as
+begin                       
+update a_products_storage set volume_pr=volume_pr-volume_pr1 where id_product_storage=product;
+commit;
+end;
 
 procedure TransferProductStorage (
       p_id_product_storage number,
-      p_id_storage number
+      p_id_storage number,
+      volume_pr1 integer
       ) as
   v_product a_products_storage%rowtype;
   v_errors varchar2(1000);
   v_exc exception;
   v_exist pls_integer;
+  v_volume a_products_storage.volume_pr%type;
   begin 
     begin
       select * into v_product from a_products_storage e where e.id_product_storage = p_id_product_storage;
@@ -72,8 +81,14 @@ procedure TransferProductStorage (
     if v_errors is null then 
       if v_product.id_storage = p_id_storage then raise v_exc;
       else 
-        update a_products_storage set id_storage = p_id_storage where id_product_storage = p_id_product_storage;
-        dbms_output.put_line('ok!'); 
+          if v_product.volume_pr>=volume_pr1 then
+          v_volume:=v_product.volume_pr-volume_pr1;
+          NewProductStorage(v_volume,v_product.id_storage,v_product.id_product);
+            update a_products_storage set id_storage = p_id_storage, volume_pr=volume_pr1 where id_product_storage = p_id_product_storage;
+            
+            dbms_output.put_line('ok!'); 
+          else dbms_output.put_line('Tovara nedostatochno'); 
+          end if;
         commit;
        end if; 
     else dbms_output.put_line(v_errors); 
@@ -83,7 +98,6 @@ procedure TransferProductStorage (
   end;
 procedure NewProductStorage(
       p_volume_pr          a_products_storage.volume_pr%type,
-      p_product_storage              number,
       p_storage           number,
       p_product           number) as
   begin 
